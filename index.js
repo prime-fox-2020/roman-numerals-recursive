2f23ea26b824c4e09463f5d4289382c0cc5fe14e
charles2901 <chrlsjnthn@gmail.com>
2020-03-31 11:07:20 +0700

    solve

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 function toRoman(num) {
+    let dictionary = [
+        ['I',  1],
+        ['IV', 4],   
+        ['V',  5],
+        ['IX', 9],
+        ['X',  10],
+        ['XL', 40],
+        ['L',  50],
+        ['XC', 90],
+        ['C',  100],
+        ['CD', 400],
+        ['D',  500],
+        ['CM', 900],
+        ['M',  1000]
+    ]
+
+    let result = ''
+    if(num === 0){
+        return result
+    }
+    for(let i = dictionary.length-1; i>=0 ; i--){
+        if(num >= dictionary[i][1]){
+            result += dictionary[i][0]
+            num -= dictionary[i][1]
+            break
+        }
+    }
+    return result + toRoman(num)
 
 }
 
