0addf233dec77451928c461dd9679ab13648ff0c
bzxnaga <bzxnaga@gmail.com>
2020-03-31 17:08:58 +0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+
 function toRoman(num) {
+    var roman = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ], actualresult = '', x = 0
+
+    if (num == 0) return ""
+    else {
+        for (var i = 0; i < roman.length; i++) {
+            while (num >= roman[i][0]) {
+                actualresult += roman[i][1]
+                return actualresult + toRoman(num - roman[i][0])
+            }
+        }
+    }
 
 }
+
 
 // Drive code
 console.log("My totally sweet testing script for new roman\n");
