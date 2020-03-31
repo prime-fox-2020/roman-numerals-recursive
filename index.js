e66f3595263e29d1eed29438e72812a65c449aa2
michaelm96 <michaelarceus96@gmail.com>
2020-03-31 15:51:00 +0700

    turn arab numbers to romawi numbers

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function toRoman(num) {
+    var newRom = "";
+    var arab = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var romawi = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
+    for (let i = 0; i < arab.length; i++) {
+        if(num >= arab[i]){
+            newRom+=romawi[i]
+            num-=arab[i]
+            return newRom + toRoman(num)
+        }
+    }
+    return newRom
 }
 
 // Drive code
