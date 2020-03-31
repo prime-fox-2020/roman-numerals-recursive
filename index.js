6bfcc6e31bf5fa2d83c0913418b274ac26656d10
Mercen1 <nurhabib102@gmail.com>
2020-03-31 11:56:21 +0700

    edit index.js solve

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function toRoman(num) {
+function toRoman(angka){
+    var tabelRomawi = [['M',1000],['CM',900],['D',500],['CD',400],
+                       ['C',100],['XC',90],['L',50],['XL',40],
+                       ['X',10],['IX',9],['V',5],['IV',4],['I',1]]
 
+    var hasil =''
+    for (let i = 0; i < tabelRomawi.length; i++) {
+        if(angka>=tabelRomawi[i][1]){
+            angka -= tabelRomawi[i][1]
+            return tabelRomawi[i][0] + toRoman(angka)
+        }
+        else if(angka==0){
+            return hasil
+        }
+    }
 }
 
 // Drive code
