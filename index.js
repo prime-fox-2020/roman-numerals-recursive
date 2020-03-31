13e50fa7133854463318f7c8e83c33e8309740b2
zuhdi <zudhy.duddy@gmail.com>
2020-03-31 14:51:21 +0700

    done roman recursive

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function toRoman(num) {
-
+  var kamus = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  
+  if(num == 0){
+    return ''
+  }
+  for (let i = 0; i < angka.length; i++) {
+    while (num >= angka[i]){
+      num -= angka[i]
+      return kamus[i] + toRoman(num)
+    }
+  }
 }
 
 // Drive code
