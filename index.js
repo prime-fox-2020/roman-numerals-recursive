3eb477ecf317c06c23946bd63006258189ae1b74
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-03-31 22:18:38 +0700

    finished romanNum

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function toRoman(num) {
+    var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romawi = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var angkaRomawi = '';
 
+    //looping angka
+        for (var i = 0; i < angka.length; i++) {
+            debugger
+            if (num >= angka[i]) {
+                angkaRomawi += romawi[i]
+                num -= angka[i]
+                debugger
+                return angkaRomawi + toRoman(num)
+                }
+        }
+        return angkaRomawi
 }
+
+
 
 // Drive code
 console.log("My totally sweet testing script for new roman\n");
