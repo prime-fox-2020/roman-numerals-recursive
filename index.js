816ba122e9f3f91c5b92dc6ccf2c8370fd78c584
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-03-31 10:51:26 +0700

    release 0

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function toRoman(num) {
+  let output = '',
+  dictionary = [[1, 'I'], [4, 'IV'], [5, 'V'], [9, 'IX'], [10, 'X'], [40, 'XL'], [50, 'L'], [90, 'XC'], [100, 'C'],
+  [400, 'CD'], [500, 'D'], [900, 'CM'], [1000, 'M']];
 
+  for (let i = dictionary.length-1; i >=0; i--) {
+    while (num >= dictionary[i][0]) {
+      output += dictionary[i][1];
+      num -= dictionary[i][0];
+    }
+  }
+
+  return output;
 }
 
 // Drive code
