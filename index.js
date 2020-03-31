821ba8aec76c9ffadcd6483359e2c16c86531c94
fadhlanfm <fafadhlanriz@gmail.com>
2020-03-31 12:24:46 +0700

    dictionary moved outside function

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 function toRoman(num) {
-    let dict = [
-      [1000, "M"],
-      [900, "CM"],
-      [500, "D"],
-      [400, "CD"],
-      [100, "C"],
-      [90, "XC"],
-      [50, "L"],
-      [40, "XL"],
-      [10, "X"],
-      [9, "IX"],
-      [5, "V"],
-      [4, "IV"],
-      [1, "I"]
-    ]
 
     let output = ''
 
@@ -26,6 +11,22 @@ function toRoman(num) {
     }
     return output
 }
+
+let dict = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+  ]
 
 // Drive code
 console.log("My totally sweet testing script for new roman\n");
