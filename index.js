f227f6da4d522cbb0295bf70d58d4e2de51281c3
fadhlanfm <fafadhlanriz@gmail.com>
2020-03-31 12:22:04 +0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function toRoman(num) {
+    let dict = [
+      [1000, "M"],
+      [900, "CM"],
+      [500, "D"],
+      [400, "CD"],
+      [100, "C"],
+      [90, "XC"],
+      [50, "L"],
+      [40, "XL"],
+      [10, "X"],
+      [9, "IX"],
+      [5, "V"],
+      [4, "IV"],
+      [1, "I"]
+    ]
 
+    let output = ''
+
+    for(let i = 0; i < dict.length; i++){
+        if(num >= dict[i][0]){
+            output += dict[i][1]
+            num -= dict[i][0]
+            return output + toRoman(num)
+        }
+    }
+    return output
 }
 
 // Drive code
