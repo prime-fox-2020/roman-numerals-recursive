aaa2939204b221c495799c7e6394d34f05939320
Agustinus Adam <agustinusadamw@gmail.com>
2020-03-31 10:38:06 +0700

    roman-numerals-recursive done

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function toRoman(num) {
+    var romawi = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
 
+    var hasil = ''
+    for (key in romawi) {
+        if (num >= romawi[key]) {
+            return hasil += key + toRoman(num - romawi[key])
+        } else if (num == 0) {
+            hasil = ''
+            return hasil
+        }
+    }
 }
 
 // Drive code
