9f6e3f713c7c09e4f7c7c7500cd0bd58acad7540
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-03-31 17:01:48 +0700

    roman numerals recursive done

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 function toRoman(num) {
+    let list = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I'],
+    ]
 
+    for(let i = 0; i < list.length; i++){
+    
+        if(num < 1){
+            return ''
+        }else if(num >= list[i][0]){
+            return list[i][1] + toRoman(num - list[i][0])
+        }  
+    }
 }
 
-// Drive code
+
 console.log("My totally sweet testing script for new roman\n");
 console.log("input | expected | actual");
 console.log("——————|——————————|———————");
