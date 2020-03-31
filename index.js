990049ccc95de2639ffe52ab463effac22d129c5
susilo hartomo <encusilo545110@gmail.com>
2020-03-31 10:37:47 +0700

    toRoman

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 function toRoman(num) {
+    var angkaRomawi = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]]
+
+    var hasil = ''
+
+    //Non Recursive
+    // for (let i = 0; i < angkaRomawi.length; i++) {
+    //     while (num >= angkaRomawi[i][1]) {
+    //         hasil += angkaRomawi[i][0]
+    //         num -= angkaRomawi[i][1]
+    //     }
+    // }
+    // return hasil
+
+    //Recursive
+    for (let i = 0; i < angkaRomawi.length; i++) {
+        if (num >= angkaRomawi[i][1]) {
+            return hasil += angkaRomawi[i][0] + toRoman(num - angkaRomawi[i][1])
+        } else if (num == 0) {
+            return ''
+        }
+    }
+
 
 }
 
