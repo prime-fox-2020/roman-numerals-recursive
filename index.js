682d1ed22565123aaffa5d6103bfb993c2b5358a
Adib Faqih <foezst@gmail.com>
2020-03-31 15:03:50 +0700

    roman recursive solved

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 function toRoman(num) {
-
+  var romans = [
+    [1, 'I'],
+    [5, 'V'],
+    [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M']
+  ];
+  var rom = '', len = 0;
+  if (num == 0) {
+    return rom;
+  }
+  
+  for (var i = 0; i < romans.length; i++) {
+    if (num >= romans[i][0]) {
+      rom = romans[i][1];
+      len = romans[i][0];
+    } else if (i>0 && i % 2 != 0 && romans[i][0] - romans[i-1][0] <= num) {
+      rom = romans[i-1][1] + romans[i][1];
+      len = romans[i][0] - romans[i-1][0];
+    } else if (i>1 && i % 2 == 0 && romans[i][0] - romans[i-2][0] <= num) {
+      rom = romans[i-2][1] + romans[i][1];
+      len = romans[i][0] - romans[i-2][0];
+    }
+  }
+  return rom + toRoman(num - len);
 }
 
 // Drive code
