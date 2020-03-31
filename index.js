88a175a8b9543ae5805382b141d0582f322c6177
chabode <rizqa.aulia@gmail.com>
2020-03-31 20:12:21 +0700

    solved recursive roman

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 function toRoman(num) {
+    let result = ''
+    var romans = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ]
 
+    if (num > 3000){
+        return 'Number exceed limit'
+    }
+    for(var a = 0; a<romans.length; a++){
+        if (num < 1){
+            return ''
+        } else if (num >= romans[a][0]){
+            return romans[a][1] + toRoman(num - romans[a][0])
+        }
+    }
 }
 
 // Drive code
