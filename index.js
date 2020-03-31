e1d3f7738ee30a51fe69c6cde92e180fa54c1ebc
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-31 12:25:15 +0700

    0

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 function toRoman(num) {
+    
+    //let result = ''
+    if (num >= 1000) {
+        return 'M' + toRoman(num % 1000)
+    } else if (num >= 900) {
+        return 'CM' + toRoman(num % 900)
+    } else if (num >= 500) {
+        return 'D' + toRoman(num % 500)
+    } else if (num >= 400) {
+        return 'CD' + toRoman(num % 400)
+    } else if (num >= 100) {
+        return 'C' + toRoman(num % 100)
+    } else if (num >= 90) {
+        return 'XC' + toRoman(num % 90)
+    } else if (num >= 50) {
+        return 'L' + toRoman(num % 50)
+    } else if (num >= 40) {
+        return 'XL' + toRoman(num % 40)
+    } else if (num >= 10) {
+        return 'X' + toRoman(num % 10)
+    } else if (num >= 9) {
+        return 'IX' + toRoman(num % 9)
+    } else if (num >= 5) {
+        return 'V' + toRoman(num % 5)
+    } else if (num >= 4) {
+        return 'IV' + toRoman(num % 4)
+    } else if (num >= 1) {
+        return 'I' + toRoman(num - 1)
+    } else if (num === 0) {
+        return ''
+    }
 
 }
 
