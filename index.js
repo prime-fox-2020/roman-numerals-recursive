5b0bed71aa9c9fffdd15005c3e5dc6e0e0bd9f43
Samirudin242 <samtheyaku@gmail.com>
2020-03-31 16:23:26 +0700

    roman-numerals-recursive done

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function toRoman(num) {
+    if(num > 3000) {
+        return 'Input angka maksimal 3000';
+    }
+    var romanRole = [
+        [1,'I'],
+        [2, 'II'],
+        [3,'III'],
+        [4,'IV'],
+        [5,'V'],
+        [9, 'IX'],
+        [10,'X'],
+        [40, 'XL'],
+        [50,'L'],
+        [100,'C'],
+        [400, 'CD'],
+        [500,'D'],
+        [1000,'M']
+    ]
 
+    for(var i = romanRole.length - 1; i >=0; i--) {
+        if(num < 1) {
+            return '';
+        } else if (num >= romanRole[i][0]) {
+            return romanRole[i][1] + toRoman(num - romanRole[i][0]);
+        }
+    }
 }
 
 // Drive code
