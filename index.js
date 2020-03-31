f8137a2d0d7956f375eb303497b6c678001fb10d
syukur <syukurbaru@gmail.com>
2020-03-31 11:07:02 +0700

    add file roman numeral recursive

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function toRoman(num) {
 
+    var array = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ]
+    if (num > 3000) return 'Maksimal 3000';
+
+    for (var i = 0; i < array.length; i++) {
+        if (num < 1) return '';
+        else if (num >= array[i][0]) {
+            // console.log(array[i][1]);
+            return array[i][1] + toRoman(num - array[i][0])
+        }
+    }
+
 }
+
 
 // Drive code
 console.log("My totally sweet testing script for new roman\n");
