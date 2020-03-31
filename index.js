812f9e3c409a75ad7fe52e61f5123e12cb760465
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-03-31 21:54:45 +0700

    airell

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function toRoman(num) {
+  const romawi = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+	const arab = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+	let res = '';
 
+	for (var i = 0; i < arab.length; i++) {
+		if (num >= arab[i]) {
+			res += romawi[i];
+			num -= arab[i];
+			return res + toRoman(num);
+		}
+	}
+
+	return res;
 }
 
 // Drive code
