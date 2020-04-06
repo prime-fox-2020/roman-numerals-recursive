af848d2a8fb95469c9fd05b696b548bd16bcc053
permaficus <abukhalif2019@gmail.com>
2020-04-06 21:15:12 +0700

    Roman numerals with recursive is done!

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function toRoman(num) {
+    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'], 
+    numeral = '',
+    f = 0;
+    
+    for (var i = 0; i < decimal.length; i++){
+        if (decimal[i] <= num) {
+            numeral += roman[i];
+            num -= decimal[i];
+            f++;
+        }
+    }
 
+    return f == 0 ? numeral:numeral + toRoman(num);
 }
 
 // Drive code
